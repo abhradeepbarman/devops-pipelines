a102abd740f4165a4ab8383d023a7abd76b406fb
abhradeepbarman <abhradeep709@gmail.com>
2025-09-13 09:27:04 +0530

    Add CI pipeline

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("Hello Devops, I am learning CI/CD with github actions");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Strating test...");
+setTimeout(() => console.log("waiting 3 seconds"), 3000);
+console.log("Test completed");
